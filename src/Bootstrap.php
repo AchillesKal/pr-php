7842b72b4141f1cd484a3af66b104228e05a10f8
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-06-19 23:46:16 +0300

    requires compose autoload in the bootstrap file

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,7 @@
 <?php declare(strict_types=1);
 
-echo 'Hello, from the bootstrap file :)';
+define('ROOT_DIR', dirname(__DIR__));
+
+require ROOT_DIR . '/vendor/autoload.php';
+
+echo 'Hello from the bootstrap file :)';
