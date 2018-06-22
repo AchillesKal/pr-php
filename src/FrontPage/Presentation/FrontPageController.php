728c0766a4ff4863736427212b6892aa9adb9c21
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-06-22 09:05:54 +0300

    adds TwigTemplateRenderer

--- src/FrontPage/Presentation/FrontPageController.php
+++ src/FrontPage/Presentation/FrontPageController.php
@@ -4,9 +4,17 @@ namespace PrPHP\FrontPage\Presentation;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use PrPHP\Framework\Rendering\TemplateRenderer;
 
 final class FrontPageController
 {
+    private $templateRenderer;
+
+    public function __construct(TemplateRenderer $templateRenderer)
+    {
+        $this->templateRenderer = $templateRenderer;
+    }
+
     public function show(Request $request): Response
     {
         $content = 'Hello, ' . $request->get('name', 'visitor');
