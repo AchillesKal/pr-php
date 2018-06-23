426e684dbeebd6f9063cc974eaa718980ab755fb
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-06-23 18:11:21 +0300

    uses submission query to get submissions

--- src/FrontPage/Application/SubmissionsQuery.php
+++ src/FrontPage/Application/SubmissionsQuery.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace SocialNews\FrontPage\Application;
+namespace PrPHP\FrontPage\Application;
 
 interface SubmissionsQuery
 {
--- src/FrontPage/Presentation/FrontPageController.php
+++ src/FrontPage/Presentation/FrontPageController.php
@@ -5,24 +5,23 @@ namespace PrPHP\FrontPage\Presentation;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use PrPHP\Framework\Rendering\TemplateRenderer;
+use PrPHP\FrontPage\Application\SubmissionsQuery;
 
 final class FrontPageController
 {
     private $templateRenderer;
+    private $submissionsQuery;
 
-    public function __construct(TemplateRenderer $templateRenderer)
+    public function __construct(TemplateRenderer $templateRenderer, SubmissionsQuery $submissionsQuery)
     {
         $this->templateRenderer = $templateRenderer;
+        $this->submissionsQuery = $submissionsQuery;
     }
 
     public function show(Request $request): Response
     {
-        $submissions = [
-            ['url' => 'http://google.com', 'title' => 'Google'],
-            ['url' => 'http://bing.com', 'title' => 'Bing'],
-        ];
         $content = $this->templateRenderer->render('FrontPage.html.twig', [
-            'submissions' => $submissions,
+            'submissions' => $this->submissionsQuery->execute(),
         ]);
         return new Response($content);
     }
