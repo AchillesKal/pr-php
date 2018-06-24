989ebe8d1394c00e3e0f63655b3f2e147737c168
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-06-24 16:13:50 +0300

    Adds user registration form class

---
+++ src/User/Presentation/RegisterUserForm.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace SocialNews\User\Presentation;
+
+use PrPHP\Framework\Csrf\StoredTokenValidator;
+
+final class RegisterUserForm
+{
+
+    private $storedTokenValidator;
+    private $token;
+    private $nickname;
+    private $password;
+    public function __construct(
+        StoredTokenValidator $storedTokenValidator,
+        string $token,
+        string $nickname,
+        string $password
+    ) {
+        $this->storedTokenValidator = $storedTokenValidator;
+        $this->token = $token;
+        $this->nickname = $nickname;
+        $this->password = $password;
+    }
+}
