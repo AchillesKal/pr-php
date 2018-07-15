2480aaf069db5b0aee6719f0369dc7ac822f6133
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-07-15 21:12:01 +0300

    Adds first test

---
+++ tests/Framework/CSRF/StoredTokenReaderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\PrPHP\Framework\Csrf;
+
+use PHPUnit\Framework\TestCase;
+
+class StoredTokenReaderTest extends TestCase{
+    public function testThatYourComputerWorks()
+    {
+        $this->assertTrue(false);
+    }
+}
