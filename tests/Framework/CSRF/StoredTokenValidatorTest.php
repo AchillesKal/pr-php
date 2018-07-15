665dc813380547254ceabb20f601ef6d08ad2af8
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-07-15 23:33:24 +0300

    adds a second test

---
+++ tests/Framework/CSRF/StoredTokenValidatorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\PrPHP\Framework\Csrf;
+
+use PHPUnit\Framework\TestCase;
+
+class StoredTokenValidatorTest extends TestCase{
+    public function testThatYourComputerWorks()
+    {
+        $this->assertTrue(false);
+    }
+}
